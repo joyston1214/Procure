bfc569b91548931035f4869f391da8796c77cc24
nikhilpatel <nikhilpatel@localhost>
2023-11-28 12:34:30 +0530

    ggf

--- src/test/java/joy/test.java
+++ src/test/java/joy/test.java
@@ -3,7 +3,9 @@ package joy;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hbjds");
-		System.out.println();
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
 	}
 
 }
