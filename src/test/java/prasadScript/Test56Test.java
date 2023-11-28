5599074c98059ef96a336292edfbc5a49387b394
fjoys <fjoys@JOYSTON>
2023-11-28 12:10:14 +0530

    joy

--- src/test/java/prasadScript/Test56Test.java
+++ src/test/java/prasadScript/Test56Test.java
@@ -2,11 +2,10 @@ package prasadScript;
 
 import org.testng.annotations.Test;
 
-public class TestKaro12 {
-
+public class Test56Test {
 	@Test
 	public void Joystontest()
 	{
-		System.out.println("test12");
+		System.out.println("test 56");
 	}
 }
